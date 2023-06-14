84073ec14eab4321f7139b4be5d4b8165094815c
Nikolaus Huber <s2210824023@fhooe.at>
2023-06-14 12:44:03 +0200

    fixed bug

--- dummyandriod.java
+++ dummyandriod.java
@@ -4,7 +4,7 @@
 int i = 1234567890 //thats an integer
 >>>>>>> 596a4411ef25769dc795411f1a1b89b245416388
 
+int six = 5;
+public void thisIsANewMethod() { six = 6;}
 
-public void thisIsANewMethod() { }
-int six = 6;
 int five = 5;
