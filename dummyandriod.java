7c3b1775516c09312977accbf618c85c02817daa
Auer Simon <s2210824018@fhooe.at>
2023-06-14 13:54:04 +0200

    added bugfix for dummy android

--- dummyandriod.java
+++ dummyandriod.java
@@ -3,7 +3,11 @@ int six = 5;
 public void thisIsANewBugfixMethod() { six = 6;}
 
 int five = 5;
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {
+	//big changes 123
+	six = 4;
+	System.out.println("amazing console")
+}
 int six = 6;
 int five = 5;
 int seven = 7;
